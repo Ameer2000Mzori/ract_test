9fa419728b2c04a3811c0ece25a54af6269e916c
Ameer Ameen <ameerapex496@gmail.com>
2023-06-03 06:04:12 +0200

    added main and fixed some issues with navbar

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Getting Started with  React my first website
- *free to use repo anytime you need! :D
+# Getting Started with  React my first website 0.0.1-BETA
+ free to use repo anytime you need! :D
 
-# Getting start first with navBar Tag 1.1.0
-*alomst finshed with nav-bar {next update should be responsive navbar}
+# Getting start first with navBar Tag 1.1.0-BETA
+alomst finshed with nav-bar {next update should be responsive navbar}
+
+# Finshed work with navbar 1.2.0-BETA
+
+# Working with main 1.3.0-BETA
+
---
+++ src/components/main.js
@@ -0,0 +1,23 @@
+import "./main.css";
+
+function main() {
+    return (
+      <>        
+      <section className="main-1">
+        <section className="main">
+            <div>
+            <img className="main-img"src="https://wallpaperaccess.com/full/42271.jpg"></img>
+            </div>
+            <div>
+            <p className="mian-p" ><strong>Watchfire :</strong> A blazing beacon illuminating the night, signaling danger or calling for aid, igniting tales of adventure and camaraderie."</p>
+            <a className="main-a" href="#">Buy on steam now </a>
+            </div>
+            <div>
+            </div>
+        </section>
+        </section>
+      </>
+    );
+  }
+  
+  export default main;
