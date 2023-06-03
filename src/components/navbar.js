de1b0577419693e8d03acd6fffac53a54dbe469a
Ameer Ameen <ameerapex496@gmail.com>
2023-06-03 05:07:46 +0200

    added responsive changes to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,27 @@
-import './navbar.css';
+import "./navbar.css";
 
-function navbar(){
-    return (
-        <>
-        <nav className='navBar'>
+
+function navbar() {
+  return (
+    <>
+      <nav className="navBar">
         <div className="navBrand">IGN</div>
-        <a href='#' className='toggleBtn'>
-            <span className='Bars'></span>
-            <span className='Bars'></span>
-            <span className='Bars'></span>
-        </a>
         <div className="navLinks">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">contact</a></li>
-
-            </ul>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+          </ul>
         </div>
-        </nav>
-        </>
-    )
+      </nav>
+    </>
+  );
 }
 
 export default navbar;
