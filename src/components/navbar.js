0490c0be0f842bc2bff82127defe0d081c28786b
Ameer Ameen <ameerapex496@gmail.com>
2023-06-03 03:17:21 +0200

    the start of my test website

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import './navbar.css';
+
+function navbar(){
+    return (
+        <>
+        <nav className='navBar'>
+        <div className="navBrand">IGN</div>
+        <a href='toggleBtn'>
+            <span className='Bars'></span>
+            <span className='Bars'></span>
+            <span className='Bars'></span>
+        </a>
+        <div className="navLinks">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">contact</a></li>
+
+            </ul>
+        </div>
+        </nav>
+        </>
+    )
+}
+
+export default navbar;
