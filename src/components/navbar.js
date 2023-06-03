bc6a5f76d4f6be412fbd1aca15859dd80c652234
Ameer Ameen <ameerapex496@gmail.com>
2023-06-04 01:35:38 +0200

    adding navbar logics

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,10 +2,20 @@ import "./navbar.css";
 
 
 function navbar() {
+    // let TogBtn = document.getElementsByClassName("toggleBtn")[0]
+    // let navLnks = document.getElementsByClassName("navLinks")[0]
+    // TogBtn.addEventListener('click', function(){
+    //     navLnks.classList.toggle("active");
+    // })
   return (
     <>
       <nav className="navBar">
         <div className="navBrand">IGN</div>
+        <a href="#" className="toggleBtn">
+          <span className="bars"></span>
+          <span className="bars"></span>
+          <span className="bars"></span>
+        </a>
         <div className="navLinks">
           <ul>
             <li>
