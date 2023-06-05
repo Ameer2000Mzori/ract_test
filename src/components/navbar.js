08baad775766fcec1cb90af075a201fed6b686d9
Ameer Ameen <ameerapex496@gmail.com>
2023-06-05 04:05:06 +0200

    added new changes in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,11 +10,16 @@ function navbar() {
   return (
     <>
       <nav className="navBar">
+<<<<<<< Updated upstream
         <div className="navBrand">IGN</div>
 
         <a
           href="#"
           onClick={clicks}className="toggleBtn">
+=======
+        <div className="navBrand"><img className="navBrand" src=".\imgs\Firewatch_Logo.png"></img></div>
+        <a href="#" onClick={clicks} className="toggleBtn">
+>>>>>>> Stashed changes
           <span className="bars"></span>
           <span className="bars"></span>
           <span className="bars"></span>
@@ -22,13 +27,13 @@ function navbar() {
         <div className="navLinks">
           <ul>
             <li>
-              <a href="#">Home</a>
+              <a href="#">HOME</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#">ABOUT</a>
             </li>
             <li>
-              <a href="#">contact</a>
+              <a href="#">CONTACT US</a>
             </li>
           </ul>
         </div>
