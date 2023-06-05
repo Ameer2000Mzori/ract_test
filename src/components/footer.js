6bc60b9796af06215da12acc8fbbd04e5e11722e
Ameer Ameen <ameerapex496@gmail.com>
2023-06-05 05:26:07 +0200

    finshed working with footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,43 @@ import "./footer.css"
 function Footer(){
     return(
         <>
+        <section className="imgs-links">
+            <h1 className="links-tittle-h1">FOLLOW US ON </h1>
+            <div className="img-face"><img className="img-footer" src="\logos\fblogo.png"></img></div>
+            <div className="img-insta"><img className="img-footer" src="\logos\instalogo.png"></img></div>
+        </section>
         <section className="footer-all">
-            <section className="footer-1">
-            <div>contact</div>
+        <section className="footer-1">
+
+            <div className="RESOURCES-footer">
+            <div className="footer-div-tittle">RESOURCES</div>
             <ul>
-                <li>why</li>
-                <li>why</li>
-                <li>why</li>
+                <li><a href="#">Appolication</a></li>
+                <li><a href="#">FAQ</a></li>
             </ul>
-            </section>
+            </div>
+            <div className="PRICING-footer">
+            <div  className="footer-div-tittle">PRICING</div>
+            <ul>
+                <li><a href="#">Overview</a></li>
+                <li><a href="#">Promotions</a></li>
+            </ul>
+            </div>
+            <div className="COMPANY-footer">
+            <div  className="footer-div-tittle">COMPANY</div>
+            <ul>
+                <li> <a href="#">About us</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+            </div>
+            <div className="SOCIAL-footer">
+            <div  className="footer-div-tittle">SOCIAL</div>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instgram</a></li>
+            </ul>
+            </div>
+        </section>
         </section>
         
         </>
